a402cf2e06c99fe914413dc46a72199e9717ded1
omurbozdag <omur8819@gmail.com>
2021-06-24 17:42:25 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <br>
+            <app-header></app-header>
+            <hr>
+            <router-view></router-view>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+    import Header from './components/Header.vue';
+    export default {
+        components: {
+            appHeader: Header,
+        }
+    }
+</script>
+
+<style>
+
+</style>
